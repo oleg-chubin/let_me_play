4d67263296ac2d5aa997810b174deffb0f180d00
Alexander Kamyanskiy <alexander.kamyanskiy@willowcode.com>
2015-02-20 00:02:01 +0300

    + some models

--- let_me_app/models.py
+++ let_me_app/models.py
@@ -16,9 +16,61 @@ class Followable(models.Model):
 
 class User(Followable):
     user = models.OneToOneField(DjangoUser)
+    group = models.ManyToManyField(Group)
+    permission = models.ManyToManyField(Permission)
+
+class InternalMessage(models.Model):
+    sender = models.ForeignKey(User, related_name='sender')
+    recipient = models.ForeignKey(User, related_name='recipient')
 
 class Peeper(models.Model):
     followable = models.ForeignKey(Followable)
+    user = models.ForeignKey(User)
+
+class PrivateComment(Followable):
+    user = models.ForeignKey(User)
+
+class Site(Followable):
+    pass
+
+class Permission(models.Model):
+    name = models.CharField(max_length=128)
+    user = models.ManyToManyField(User)
+    group = models.ManyToManyField(Group)
+
+class Group(models.Model):
+    name = models.CharField(max_length=128)
+    user = models.ManyToManyField(User)
+    permission = models.ManyToManyField(Permission)
+
+class Court(Followable):
+    site = models.ForeignKey(Site)
+    group = models.ForeignKey(Group)
+
+class Invoice(models.Model):
+    name = models.CharField(max_length=128)
+
+class Event(Followable):
+    name = models.CharField(max_length=128)
+    description = models.TextField(max_length=1024)
+    court = models.ForeignKey(Court)
+    invoice = models.ForeignKey(Invoice)
+    inventory_list = models.ForeignKey(InventoryList)
+
+class Proposal(models.Model):
+    name = models.CharField(max_length=128)
+    description = models.TextField(max_length=1024)
+    user = models.ForeignKey(User)
+    event = models.ForeignKey(Event)
+
+class Staff(User):
+    name = models.CharField(max_length=128)
+    event = models.ForeignKey(Event)
+
+class Application(models.Model):
+    name = models.CharField(max_length=128)
+    event = models.ForeignKey(Event)
+    inventory_list = models.ForeignKey(InventoryList)
     user = models.ForeignKey(User)
 
 class Receipt(models.Model):
@@ -39,5 +91,7 @@ class Inventory(models.Model):
 
 class Visit(models.Model):
     inventory_list = models.ForeignKey(InventoryList)
-    # there we need 2 fileds
+    user = models.ForeignKey(User)
+    event = models.ForeignKey(Event)
+
 
