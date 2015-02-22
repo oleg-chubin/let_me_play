df1bc635c2f978ec9fc8935ae123b80c839ba04b
Oleg Chubin <oleg.chubin@willowcode.com>
2015-02-22 16:57:05 +0300

    small navbar rework

--- let_me_app/models.py
+++ let_me_app/models.py
@@ -75,11 +75,11 @@ class User(AbstractBaseUser, Followable, PermissionsMixin):
         Returns the first_name plus the last_name, with a space in between.
         """
         full_name = '%s %s' % (self.first_name, self.last_name)
-        return full_name.strip()
+        return full_name.strip() or self.email
 
     def get_short_name(self):
         "Returns the short name for the user."
-        return self.first_name
+        return self.first_name or self.email
 
 
 class InternalMessage(models.Model):
