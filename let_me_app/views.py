58502126da73f6dc6ea75f58fdbd87fa53ebbef0
Oleg Chubin <oleg.chubin@willowcode.com>
2015-07-05 13:16:35 +0300

    #12 change timeline a bit

--- let_me_app/views.py
+++ let_me_app/views.py
@@ -1,4 +1,5 @@
 from xml.dom import minidom
+from itertools import groupby
 
 from django import http
 from django.contrib.auth.models import Group
@@ -105,11 +106,6 @@ class EventView(DetailView):
         result['active_applications'] =event.application_set.filter(
             status=models.ApplicationStatuses.ACTIVE
         )
-        result['active_visits'] =event.visit_set.filter(
-            status__in=(
-                models.VisitStatuses.PENDING, models.VisitStatuses.COMPLETED
-            )
-        )
         return result
 
 
@@ -119,7 +115,14 @@ class UserEventListView(ListView):
 
     def get_queryset(self, **kwargs):
         result = super(UserEventListView, self).get_queryset(**kwargs)
-        return result.filter(user=self.request.user)
+        return result.filter(user=self.request.user).order_by('-event__start_at')
+
+    def get_context_data(self, **kwargs):
+        result = super(UserEventListView, self).get_context_data(**kwargs)
+        object_list = result['object_list']
+        grouped_objects = groupby(object_list, lambda x: x.event.start_at.date())
+        result['grouped_objects'] = [(i, [j for j in g]) for i, g in grouped_objects]
+        return result
 
 
 class EventActionMixin(object):
