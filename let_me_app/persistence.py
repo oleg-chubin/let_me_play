afe79c37a3afbba9ece7f9380577c4485026edae
Oleg Chubin <oleg.chubin@gmail.com>
2015-08-23 00:13:21 +0300

    #73 small UX improvment

--- let_me_app/persistence.py
+++ let_me_app/persistence.py
@@ -6,45 +6,9 @@ Created on Jun 21, 2015
 from . import models
 
 
-def get_court_actions_for_user(user, court_object, is_admin=False):
-    return ['add_to_admin_group']
-
-
-def get_event_actions_for_user(user, event_object, is_admin=False):
-    if user.is_anonymous():
-        return []
-    result = []
-    proposals = models.Proposal.objects.filter(
-        user=user, event=event_object, status=models.ProposalStatuses.ACTIVE
-    )
-    applications = models.Application.objects.filter(
-        user=user, event=event_object, status=models.ApplicationStatuses.ACTIVE
-    )
-    visits = models.Visit.objects.filter(
-        user=user,
-        event=event_object,
-        status=models.VisitStatuses.PENDING
-    )
-    visit_exists = visits.exists()
-
-    admin_actions = ['propose_event', 'create_visit']
-    if is_admin:
-        result.extend(admin_actions)
-
-    if proposals.count():
-        result.extend(['decline_proposal', 'accept_proposal'])
-    if applications.count():
-        result.extend(["cancel_application"])
-    if visit_exists:
-        result.append('cancel_visit')
-    if not set(result) - set(admin_actions):
-        result.append('apply_for_event')
-    return result
-
-
 def save_event_and_related_things(event, user, visitors=tuple()):
     event.save()
-    chat = models.InternalMessage.objects.create(subject=event)
+    models.InternalMessage.objects.create(subject=event)
 
     for visitor in visitors:
         create_event_visit(event, visitor, None)
