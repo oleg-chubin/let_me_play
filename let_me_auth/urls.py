441f3069d3d535bde6c8d32fa9525f2bd9efc03f
Oleg Chubin <oleg.chubin@gmail.com>
2016-02-16 15:14:24 +0300

    fix security issue

--- let_me_auth/urls.py
+++ let_me_auth/urls.py
@@ -5,6 +5,7 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf import settings
 from let_me_auth.views import (EditUserView, ProfileDetailsView,
     CreateConfirmationCodeView, CheckConfirmationCodeView)
+from django.contrib.auth.decorators import login_required
 
 urlpatterns = patterns('let_me_auth.views',
     url(r'^home/$', 'home'),
@@ -13,14 +14,17 @@ urlpatterns = patterns('let_me_auth.views',
     url(r'^set_password/$', 'set_password', name='initial_password'),
     url(r'^email-sent/', 'validation_sent'),
     url(r'^email/$', 'require_email', name='require_email'),
-    url(r'^profile/$', ProfileDetailsView.as_view(), name='profile_details'),
-    url(r'^update/profile/$', EditUserView.as_view(), name='update_profile'),
+    url(r'^profile/$', login_required(login_url='/login/')(ProfileDetailsView.as_view()),
+        name='profile_details'),
+    url(r'^update/profile/$',
+        login_required(login_url='/login/')(EditUserView.as_view()),
+        name='update_profile'),
 
     url(r'^confirmationcode/generate/$',
-        CreateConfirmationCodeView.as_view(),
+        login_required(login_url='/login/')(CreateConfirmationCodeView.as_view()),
         name='generate_confirmation_code'),
     url(r'^confirmationcode/check/$',
-        CheckConfirmationCodeView.as_view(),
+        login_required(login_url='/login/')(CheckConfirmationCodeView.as_view()),
         name='check_confirmation_code'),
 
 )
