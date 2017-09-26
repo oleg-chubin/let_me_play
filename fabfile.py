ea12db76a231498b05850c89494e233d24ad3f46
Oleg Chubin <oleg.chubin@gmail.com>
2017-09-27 00:41:53 +0300

    update requirements

--- fabfile.py
+++ fabfile.py
@@ -1,11 +1,13 @@
-from fabric.api import run, cd, prefix 
+from fabric.api import run, cd, prefix
 
 def deploy_everything():
     with cd('/home/httpd/let_me_play/'):
         run('git stash')
         run('git pull')
         run('git stash apply')
+        run('npm install')
         with prefix('source venv/bin/activate'):
+            run('pip install -r requirements.txt')
             run('python manage.py migrate')
             run('python manage.py collectstatic')
             run('python manage.py compilemessages')
